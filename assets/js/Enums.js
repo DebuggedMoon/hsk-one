ccaa8d2cbfc795dba58af65e8e8981419b3c0dff
Debugged Moon <predojevic@proton.me>
2023-03-13 22:22:47 +0000

    🔨 add Enums.js

---
+++ assets/js/Enums.js
@@ -0,0 +1,16 @@
+
+/**
+ * An enum is a user-defined type consisting of a set of named constants called enumerators.
+ * @param {Array.<String>} enumConstants List of strings to create enumerators of.
+ */
+class Enums {
+
+    constructor(enumConstants) {
+        // Logic for creating enum objects
+    };
+
+};
+
+export {
+    
+};
