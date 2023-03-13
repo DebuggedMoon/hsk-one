bc1617c545f18cb0bcb506df66ca6137ee7125a6
Debugged Moon <predojevic@proton.me>
2023-03-13 23:51:44 +0000

    🔨 add constructor to ProgressBar

--- assets/js/ProgressBar.js
+++ assets/js/ProgressBar.js
@@ -1,11 +1,21 @@
+import { ProgressBarStatus } from "./Enums";
+
+const progressElement = document.getElementById("segmented-progress-bar");
+const segmentElements = [...progressElement.children];
 
 /**
- *  Class for managing the progress bar ui.
+ *  Singleton Class for managing the progress bar ui.
  */
 class ProgressBar {
+
     constructor() {
+        
+        this.status = ProgressBarStatus.Inactive;
+        this.round = 0;
 
     }
+
+
 }
 
 export default (new ProgressBar());
