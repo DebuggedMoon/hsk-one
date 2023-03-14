daef8436eac1e31dcee231adf7eefc9224c0b34a
Debugged Moon <predojevic@proton.me>
2023-03-14 01:47:31 +0000

    🔧 add word-list.js

---
+++ assets/js/data/word-list.js
@@ -0,0 +1,767 @@
+export default [
+    {
+      "hanzi": "&#x6211",
+      "pinyin": "wǒ",
+      "english": "I,me"
+    },
+    {
+      "hanzi": "&#x6211&#x4eec",
+      "pinyin": "wǒmen",
+      "english": "we,us(pl.)"
+    },
+    {
+      "hanzi": "&#x4f60",
+      "pinyin": "nǐ",
+      "english": "you"
+    },
+    {
+      "hanzi": "&#x4f60&#x4eec",
+      "pinyin": "nǐmen",
+      "english": "you(pl.)"
+    },
+    {
+      "hanzi": "&#x4ed6",
+      "pinyin": "tā",
+      "english": "he,him"
+    },
+    {
+      "hanzi": "&#x5979",
+      "pinyin": "tā",
+      "english": "she,her"
+    },
+    {
+      "hanzi": "&#x4ed6&#x4eec",
+      "pinyin": "tāmen",
+      "english": "they(male+female/male,pl.)"
+    },
+    {
+      "hanzi": "&#x5979&#x4eec",
+      "pinyin": "tāmen",
+      "english": "they(females,pl.)"
+    },
+    {
+      "hanzi": "&#x8fd9",
+      "pinyin": "zhè",
+      "english": "this(here)"
+    },
+    {
+      "hanzi": "&#x90a3",
+      "pinyin": "nà",
+      "english": "that(there)"
+    },
+    {
+      "hanzi": "&#x54ea&#xff08&#x54ea&#x513f&#xff09",
+      "pinyin": "nǎ",
+      "english": "(nǎr)where"
+    },
+    {
+      "hanzi": "&#x8c01",
+      "pinyin": "shéi",
+      "english": "who"
+    },
+    {
+      "hanzi": "&#x4ec0&#x4e48",
+      "pinyin": "shénme",
+      "english": "what,why"
+    },
+    {
+      "hanzi": "&#x591a&#x5c11",
+      "pinyin": "duōshǎo",
+      "english": "howmany,howmuch"
+    },
+    {
+      "hanzi": "&#x51e0",
+      "pinyin": "jǐ",
+      "english": "afew,howmany"
+    },
+    {
+      "hanzi": "&#x600e&#x4e48",
+      "pinyin": "zěnme",
+      "english": "how"
+    },
+    {
+      "hanzi": "&#x600e&#x4e48&#x6837",
+      "pinyin": "zěnmeyàng",
+      "english": "howabout"
+    },
+    {
+      "hanzi": "&#x4e00",
+      "pinyin": "yī",
+      "english": "one"
+    },
+    {
+      "hanzi": "&#x4e8c",
+      "pinyin": "èr",
+      "english": "two"
+    },
+    {
+      "hanzi": "&#x4e09",
+      "pinyin": "sān",
+      "english": "three"
+    },
+    {
+      "hanzi": "&#x56db",
+      "pinyin": "sì",
+      "english": "four"
+    },
+    {
+      "hanzi": "&#x4e94",
+      "pinyin": "wǔ",
+      "english": "five"
+    },
+    {
+      "hanzi": "&#x516d",
+      "pinyin": "liù",
+      "english": "six"
+    },
+    {
+      "hanzi": "&#x4e03",
+      "pinyin": "qī",
+      "english": "seven"
+    },
+    {
+      "hanzi": "&#x516b",
+      "pinyin": "bā",
+      "english": "eight"
+    },
+    {
+      "hanzi": "&#x4e5d",
+      "pinyin": "jiǔ",
+      "english": "nine"
+    },
+    {
+      "hanzi": "&#x5341",
+      "pinyin": "shí",
+      "english": "ten"
+    },
+    {
+      "hanzi": "&#x96f6",
+      "pinyin": "líng",
+      "english": "zero"
+    },
+    {
+      "hanzi": "&#x4e2a",
+      "pinyin": "gè",
+      "english": "one,a,an"
+    },
+    {
+      "hanzi": "&#x5c81",
+      "pinyin": "suì",
+      "english": "year"
+    },
+    {
+      "hanzi": "&#x672c",
+      "pinyin": "běn",
+      "english": "volume"
+    },
+    {
+      "hanzi": "&#x4e9b",
+      "pinyin": "xiē",
+      "english": "some"
+    },
+    {
+      "hanzi": "&#x5757",
+      "pinyin": "kuài",
+      "english": "piece"
+    },
+    {
+      "hanzi": "&#x4e0d",
+      "pinyin": "bù",
+      "english": "no"
+    },
+    {
+      "hanzi": "&#x6ca1",
+      "pinyin": "méi",
+      "english": "no"
+    },
+    {
+      "hanzi": "&#x5f88",
+      "pinyin": "hěn",
+      "english": "quite,very"
+    },
+    {
+      "hanzi": "&#x592a",
+      "pinyin": "tài",
+      "english": "too"
+    },
+    {
+      "hanzi": "&#x90fd",
+      "pinyin": "dōu",
+      "english": "all"
+    },
+    {
+      "hanzi": "&#x548c",
+      "pinyin": "hé",
+      "english": "and"
+    },
+    {
+      "hanzi": "&#x5728",
+      "pinyin": "zài",
+      "english": "in,at"
+    },
+    {
+      "hanzi": "&#x7684",
+      "pinyin": "de\n",
+      "english": ""
+    },
+    {
+      "hanzi": "&#x4e86",
+      "pinyin": "le\n",
+      "english": ""
+    },
+    {
+      "hanzi": "&#x5417",
+      "pinyin": "ma\n",
+      "english": ""
+    },
+    {
+      "hanzi": "&#x5462",
+      "pinyin": "ne\n",
+      "english": ""
+    },
+    {
+      "hanzi": "&#x5582",
+      "pinyin": "wèi",
+      "english": "hello"
+    },
+    {
+      "hanzi": "&#x5bb6",
+      "pinyin": "jiā",
+      "english": "home"
+    },
+    {
+      "hanzi": "&#x5b66&#x6821",
+      "pinyin": "xuéxiào",
+      "english": "school"
+    },
+    {
+      "hanzi": "&#x996d&#x9986",
+      "pinyin": "fàndiàn",
+      "english": "restaurant"
+    },
+    {
+      "hanzi": "&#x5546&#x5e97",
+      "pinyin": "shāngdiàn",
+      "english": "store"
+    },
+    {
+      "hanzi": "&#x533b&#x9662",
+      "pinyin": "yīyuàn",
+      "english": "hospital"
+    },
+    {
+      "hanzi": "&#x706b&#x8f66&#x7ad9",
+      "pinyin": "huǒchēzhàn",
+      "english": "trainstation"
+    },
+    {
+      "hanzi": "&#x4e2d&#x56fd",
+      "pinyin": "Zhōngguò",
+      "english": "China"
+    },
+    {
+      "hanzi": "&#x5317&#x4eac",
+      "pinyin": "Běijīng",
+      "english": "Beijing"
+    },
+    {
+      "hanzi": "&#x4e0a",
+      "pinyin": "shàng",
+      "english": "up"
+    },
+    {
+      "hanzi": "&#x4e0b",
+      "pinyin": "xià",
+      "english": "down"
+    },
+    {
+      "hanzi": "&#x524d&#x9762",
+      "pinyin": "qiánmiàn",
+      "english": "front"
+    },
+    {
+      "hanzi": "&#x540e&#x9762",
+      "pinyin": "hòumiàn",
+      "english": "behind"
+    },
+    {
+      "hanzi": "&#x91cc",
+      "pinyin": "lǐmiàn",
+      "english": "inside"
+    },
+    {
+      "hanzi": "&#x4eca&#x5929",
+      "pinyin": "jīntiān",
+      "english": "today"
+    },
+    {
+      "hanzi": "&#x660e&#x5929",
+      "pinyin": "míngtiān",
+      "english": "tomorrow"
+    },
+    {
+      "hanzi": "&#x6628&#x5929",
+      "pinyin": "zuótiān",
+      "english": "yesterday"
+    },
+    {
+      "hanzi": "&#x4e0a&#x5348",
+      "pinyin": "shàngwǔ",
+      "english": "morning"
+    },
+    {
+      "hanzi": "&#x4e2d&#x5348",
+      "pinyin": "zhōngwǔ",
+      "english": "noon"
+    },
+    {
+      "hanzi": "&#x4e0b&#x5348",
+      "pinyin": "xiàwǔ",
+      "english": "afternoon"
+    },
+    {
+      "hanzi": "&#x5e74",
+      "pinyin": "nián",
+      "english": "year"
+    },
+    {
+      "hanzi": "&#x6708",
+      "pinyin": "yuè",
+      "english": "month"
+    },
+    {
+      "hanzi": "&#x65e5",
+      "pinyin": "rì",
+      "english": "day"
+    },
+    {
+      "hanzi": "&#x661f&#x671f",
+      "pinyin": "xīngqī",
+      "english": "week"
+    },
+    {
+      "hanzi": "&#x70b9",
+      "pinyin": "diǎn",
+      "english": "dot,spot"
+    },
+    {
+      "hanzi": "&#x5206&#x949f",
+      "pinyin": "fēnzhōng",
+      "english": "minute"
+    },
+    {
+      "hanzi": "&#x73b0&#x5728",
+      "pinyin": "xiànzài",
+      "english": "now"
+    },
+    {
+      "hanzi": "&#x65f6&#x5019",
+      "pinyin": "shíhou",
+      "english": "time"
+    },
+    {
+      "hanzi": "&#x7238&#x7238",
+      "pinyin": "bàba",
+      "english": "father"
+    },
+    {
+      "hanzi": "&#x5988&#x5988",
+      "pinyin": "māma",
+      "english": "mother"
+    },
+    {
+      "hanzi": "&#x513f&#x5b50",
+      "pinyin": "érzi",
+      "english": "son"
+    },
+    {
+      "hanzi": "&#x5973&#x513f",
+      "pinyin": "nǚér",
+      "english": "daughter"
+    },
+    {
+      "hanzi": "&#x8001&#x5e08",
+      "pinyin": "lǎoshī",
+      "english": "teacher"
+    },
+    {
+      "hanzi": "&#x5b66&#x751f",
+      "pinyin": "xuéshēng",
+      "english": "student"
+    },
+    {
+      "hanzi": "&#x540c&#x5b66",
+      "pinyin": "tóngxué",
+      "english": "classmate"
+    },
+    {
+      "hanzi": "&#x670b&#x53cb",
+      "pinyin": "péngyǒu",
+      "english": "friend"
+    },
+    {
+      "hanzi": "&#x533b&#x751f",
+      "pinyin": "yīshēng",
+      "english": "doctor"
+    },
+    {
+      "hanzi": "&#x5148&#x751f",
+      "pinyin": "xiānsheng",
+      "english": "sir"
+    },
+    {
+      "hanzi": "&#x5c0f&#x59d0",
+      "pinyin": "xiǎojiě",
+      "english": "Miss"
+    },
+    {
+      "hanzi": "&#x8863&#x670d",
+      "pinyin": "yīfu",
+      "english": "cloth"
+    },
+    {
+      "hanzi": "&#x6c34",
+      "pinyin": "shuǐ",
+      "english": "water"
+    },
+    {
+      "hanzi": "&#x83dc",
+      "pinyin": "cài",
+      "english": "dish"
+    },
+    {
+      "hanzi": "&#x7c73&#x996d",
+      "pinyin": "mǐfàn",
+      "english": "rice"
+    },
+    {
+      "hanzi": "&#x6c34&#x679c",
+      "pinyin": "shuǐguǒ",
+      "english": "fruit"
+    },
+    {
+      "hanzi": "&#x82f9&#x679c",
+      "pinyin": "píngguǒ",
+      "english": "apple"
+    },
+    {
+      "hanzi": "&#x8336",
+      "pinyin": "chá",
+      "english": "tea"
+    },
+    {
+      "hanzi": "&#x676f&#x5b50",
+      "pinyin": "bēizi",
+      "english": "cup"
+    },
+    {
+      "hanzi": "&#x94b1",
+      "pinyin": "qián",
+      "english": "money"
+    },
+    {
+      "hanzi": "&#x98de&#x673a",
+      "pinyin": "fēijī",
+      "english": "airplane"
+    },
+    {
+      "hanzi": "&#x51fa&#x79df&#x8f66",
+      "pinyin": "chūzūchē",
+      "english": "taxi"
+    },
+    {
+      "hanzi": "&#x7535&#x89c6",
+      "pinyin": "diànshì",
+      "english": "television"
+    },
+    {
+      "hanzi": "&#x7535&#x8111",
+      "pinyin": "diànnǎo",
+      "english": "computer"
+    },
+    {
+      "hanzi": "&#x7535&#x5f71",
+      "pinyin": "diànyǐng",
+      "english": "movie"
+    },
+    {
+      "hanzi": "&#x5929&#x6c14",
+      "pinyin": "tiānqì",
+      "english": "weather"
+    },
+    {
+      "hanzi": "&#x732b",
+      "pinyin": "māo",
+      "english": "cat"
+    },
+    {
+      "hanzi": "&#x72d7",
+      "pinyin": "gǒu",
+      "english": "dog"
+    },
+    {
+      "hanzi": "&#x4e1c&#x897f",
+      "pinyin": "dōngxi",
+      "english": "thing"
+    },
+    {
+      "hanzi": "&#x4eba",
+      "pinyin": "rén",
+      "english": "person"
+    },
+    {
+      "hanzi": "&#x540d&#x5b57",
+      "pinyin": "míngzi",
+      "english": "name"
+    },
+    {
+      "hanzi": "&#x4e66",
+      "pinyin": "shū",
+      "english": "book"
+    },
+    {
+      "hanzi": "&#x6c49&#x8bed",
+      "pinyin": "hànyǔ",
+      "english": "mandarinChinese"
+    },
+    {
+      "hanzi": "&#x5b57",
+      "pinyin": "zì",
+      "english": "character"
+    },
+    {
+      "hanzi": "&#x684c&#x5b50",
+      "pinyin": "zhuōzi",
+      "english": "desk"
+    },
+    {
+      "hanzi": "&#x6905&#x5b50",
+      "pinyin": "yǐzi",
+      "english": "chair"
+    },
+    {
+      "hanzi": "&#x8c22&#x8c22",
+      "pinyin": "xièxie",
+      "english": "thank"
+    },
+    {
+      "hanzi": "&#x4e0d&#x5ba2&#x6c14",
+      "pinyin": "búkèqì",
+      "english": "youarewelcome"
+    },
+    {
+      "hanzi": "&#x518d&#x89c1",
+      "pinyin": "zàijiàn",
+      "english": "good-bye"
+    },
+    {
+      "hanzi": "&#x8bf7",
+      "pinyin": "qǐng",
+      "english": "please"
+    },
+    {
+      "hanzi": "&#x5bf9&#x4e0d&#x8d77",
+      "pinyin": "duìbùqǐ",
+      "english": "sorry"
+    },
+    {
+      "hanzi": "&#x6ca1&#x5173&#x7cfb",
+      "pinyin": "méiguānxì",
+      "english": "Itdoesn'tmatter"
+    },
+    {
+      "hanzi": "&#x662f",
+      "pinyin": "shì",
+      "english": "be(am,is,are)"
+    },
+    {
+      "hanzi": "&#x6709",
+      "pinyin": "yǒu",
+      "english": "have"
+    },
+    {
+      "hanzi": "&#x770b",
+      "pinyin": "kàn",
+      "english": "look"
+    },
+    {
+      "hanzi": "&#x542c",
+      "pinyin": "tīng",
+      "english": "listen"
+    },
+    {
+      "hanzi": "&#x8bf4&#x8bdd",
+      "pinyin": "shuōhuà",
+      "english": "speak"
+    },
+    {
+      "hanzi": "&#x8bfb",
+      "pinyin": "dú",
+      "english": "read"
+    },
+    {
+      "hanzi": "&#x5199",
+      "pinyin": "xiě",
+      "english": "write"
+    },
+    {
+      "hanzi": "&#x770b&#x89c1",
+      "pinyin": "kànjiàn",
+      "english": "see"
+    },
+    {
+      "hanzi": "&#x53eb",
+      "pinyin": "jiào",
+      "english": "call"
+    },
+    {
+      "hanzi": "&#x6765",
+      "pinyin": "lái",
+      "english": "come"
+    },
+    {
+      "hanzi": "&#x56de",
+      "pinyin": "huí",
+      "english": "return"
+    },
+    {
+      "hanzi": "&#x53bb",
+      "pinyin": "qù",
+      "english": "go"
+    },
+    {
+      "hanzi": "&#x5403",
+      "pinyin": "chī",
+      "english": "eat"
+    },
+    {
+      "hanzi": "&#x559d",
+      "pinyin": "hē",
+      "english": "drink"
+    },
+    {
+      "hanzi": "&#x7761&#x89c9",
+      "pinyin": "shuìjiào",
+      "english": "sleep"
+    },
+    {
+      "hanzi": "&#x6253&#x7535&#x8bdd",
+      "pinyin": "dǎdiànhuà",
+      "english": "callup"
+    },
+    {
+      "hanzi": "&#x505a",
+      "pinyin": "zuò",
+      "english": "do"
+    },
+    {
+      "hanzi": "&#x4e70",
+      "pinyin": "mǎi",
+      "english": "buy"
+    },
+    {
+      "hanzi": "&#x5f00",
+      "pinyin": "kāi",
+      "english": "open"
+    },
+    {
+      "hanzi": "&#x5750",
+      "pinyin": "zuò",
+      "english": "sit"
+    },
+    {
+      "hanzi": "&#x4f4f",
+      "pinyin": "zhù",
+      "english": "live"
+    },
+    {
+      "hanzi": "&#x5b66&#x4e60",
+      "pinyin": "xuéxí",
+      "english": "study"
+    },
+    {
+      "hanzi": "&#x5de5&#x4f5c",
+      "pinyin": "gōngzuò",
+      "english": "work"
+    },
+    {
+      "hanzi": "&#x4e0b&#x96e8",
+      "pinyin": "xiàyǔ",
+      "english": "rain"
+    },
+    {
+      "hanzi": "&#x7231",
+      "pinyin": "ài",
+      "english": "love"
+    },
+    {
+      "hanzi": "&#x559c&#x6b22",
+      "pinyin": "xǐhuān",
+      "english": "love,like"
+    },
+    {
+      "hanzi": "&#x60f3",
+      "pinyin": "xiǎng",
+      "english": "want"
+    },
+    {
+      "hanzi": "&#x8ba4&#x8bc6",
+      "pinyin": "rènshi",
+      "english": "know"
+    },
+    {
+      "hanzi": "&#x4f1a",
+      "pinyin": "huì",
+      "english": "can"
+    },
+    {
+      "hanzi": "&#x80fd",
+      "pinyin": "néng",
+      "english": "can,beableto"
+    },
+    {
+      "hanzi": "&#x597d",
+      "pinyin": "hǎo",
+      "english": "good"
+    },
+    {
+      "hanzi": "&#x5927",
+      "pinyin": "dà",
+      "english": "big"
+    },
+    {
+      "hanzi": "&#x5c0f",
+      "pinyin": "xiǎo",
+      "english": "small"
+    },
+    {
+      "hanzi": "&#x591a",
+      "pinyin": "duō",
+      "english": "many,much"
+    },
+    {
+      "hanzi": "&#x5c11",
+      "pinyin": "shǎo",
+      "english": "few,little"
+    },
+    {
+      "hanzi": "&#x51b7",
+      "pinyin": "lěng",
+      "english": "cold"
+    },
+    {
+      "hanzi": "&#x70ed",
+      "pinyin": "rè",
+      "english": "hot"
+    },
+    {
+      "hanzi": "&#x9ad8&#x5174",
+      "pinyin": "gāoxìng",
+      "english": "happy"
+    },
+    {
+      "hanzi": "&#x6f02&#x4eae",
+      "pinyin": "piàoliang",
+      "english": "beautifu"
+    }
+];
