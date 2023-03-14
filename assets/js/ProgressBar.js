c4ca5556afc8133bd3390f524e0bc23b1d6d131d
Debugged Moon <predojevic@proton.me>
2023-03-14 00:21:37 +0000

    🔨 add setStatus to ProgressBar

--- assets/js/ProgressBar.js
+++ assets/js/ProgressBar.js
@@ -1,7 +1,11 @@
-import { ProgressBarStatus } from "./Enums";
+import { ProgressBarStatus, SegmentState} from "./Enums";
 
 const progressElement = document.getElementById("segmented-progress-bar");
 const segmentElements = [...progressElement.children];
+
+const ACTIVE_SEGMENT_CLASS = "active-segment";
+const LOST_SEGMENT_CLASS = "lost-segment";
+const WON_SEGMENT_CLASS = "won-segment";
 
 /**
  *  Singleton Class for managing the progress bar ui.
@@ -15,7 +19,39 @@ class ProgressBar {
 
     }
 
+    /**
+     * Set the progress bars status.
+     * @param {ProgressBarStatus} newStatus Status to be set.
+     */
+    setStatus(newStatus) {
 
-}
+        if (this.status === newStatus) return;
+        this.status = newStatus;
+        
+        switch(newStatus) {
+
+            case ProgressBarStatus.Inactive:
+
+                this.round = 0;
+                segmentElements.forEach( segment => {
+                    segment.classList.remove(
+                        ACTIVE_SEGMENT_CLASS,
+                        LOST_SEGMENT_CLASS,
+                        WON_SEGMENT_CLASS
+                    )
+                });
+
+            case ProgressBarStatus.Active:
+
+                segmentElements[this.round].classList
+                    .add(ACTIVE_SEGMENT_CLASS);
+
+                this.round += 1;
+
+        };
+
+    };
+
+};
 
 export default (new ProgressBar());
