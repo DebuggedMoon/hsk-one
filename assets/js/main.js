0e443f9574018d3c37fbf5c3387fa439f41f9bac
Debugged Moon <predojevic@proton.me>
2023-03-14 08:51:20 +0000

    🔨 add page content managment logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 import GameManager from "./GameManager.js";
+import { GameStatus } from "./Enums.js";
 
-GameManager.startGame()
+const answerButtons = document.getElementsByClassName("answer-button");
+
+/* Adds EventListeners to interactable elements whenever the DOM's loaded. */
+document.addEventListener("DOMContentLoaded", function () {
+    
+    /* Auto starts until menu is implemented */
+    GameManager.startGame();
+
+    for (let button of answerButtons) {
+
+        button.addEventListener("click", () => {
+
+            if (GameManager.status == GameStatus.Active) GameManager.submitUserAnswer(button.innerHTML);
+
+        })
+
+    }
+
+});
